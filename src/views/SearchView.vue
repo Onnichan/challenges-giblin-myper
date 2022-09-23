b7b80b19bda13c8ef18b4d7af4ab6f859f40f008
Daniel Aguilar <dyangeltk17@gmail.com>
2022-09-23 01:40:09 -0500

    :sparkles: create a search by title, director, date, score, image field

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,42 +1,15 @@
 <script setup>
 import TemplateSearchFilm from "../components/templates/TemplateSearchFilm.vue";
-import {
-  computed,
-  onBeforeMount,
-  onBeforeUpdate,
-  onMounted,
-  onUnmounted,
-  onUpdated,
-  ref,
-  watch,
-} from "vue";
+import { computed } from "vue";
 import { useRoute } from "vue-router";
 import { useFilmStore } from "../stores/films.store";
-import router from "../router";
 
-const search = ref([]);
 const route = useRoute();
 const { filterFilms } = useFilmStore();
-
-// onMounted(() => {
-//   console.log("Before mount", route.query.query);
-//   search.value = filterFilms("Ponyo");
-//   console.log("Before mount", search.value);
-// });
-
-// onUnmounted(() => {
-//   console.log('unmounted');
-// })
-
-// watch(route.query.query, (newSearch, oldSearch) => {
-//   console.log("updated");
-//   newSearch = filterFilms(route.query.query);
-// });
 
 const filteredFilms = computed(() => {
   console.log("computed");
   console.log(route.query.query);
-  // if (router.query.query) {
   return filterFilms(route.query.query);
 });
 </script>
